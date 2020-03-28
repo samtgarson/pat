30051b717a07997fc4b3d099d4431640eea99071
Sam Garson <samtgarson@gmail.com>
2020-03-28 12:38:09 +0000

    Use error boundary at root

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,26 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, Fragment } from 'react'
 import { AppOptions } from '@/types/options'
 import { GlobalState } from '@/src/services/global-context/index'
 import { Runner } from '@/src/components/runner'
+import ErrorBoundary, { FallbackProps } from 'react-error-boundary'
+import { ErrorMessage } from '@/src/components/util/error'
+import { Text } from 'ink'
+
+const debug = !!process.env.PAT_DEBUG
+
+const ErrorHandler: FunctionComponent<FallbackProps> = ({ componentStack, error }) => (
+  <Fragment>
+    <ErrorMessage error={error} raw={debug} />
+    { debug && <Text>{ componentStack }</Text> }
+  </Fragment>
+)
 
 const App: FunctionComponent<AppOptions> = () => {
-  return <GlobalState.Provider>
-    <Runner />
-  </GlobalState.Provider>
+  return <ErrorBoundary FallbackComponent={ErrorHandler}>
+    <GlobalState.Provider>
+      <Runner />
+    </GlobalState.Provider>
+  </ErrorBoundary>
 }
 
 export default App
